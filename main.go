d1a737ba6fa6f091cfc42b08e5d70b11312b6a27
tarcisio <tarcisio.sassara@gmail.com>
2019-11-28 00:14:24 -0300

    first release

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+
+	if len(os.Args) < 3 {
+		log.Fatal("informar o user ID do dono do bot e o token")
+	}
+
+	dono, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	tkn := os.Args[2]
+
+	b, err := tb.NewBot(tb.Settings{
+		Token:  tkn,
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	b.Handle(tb.OnText, func(m *tb.Message) {
+		if m.Sender.ID == dono {
+			index := strings.Index(m.Text, " ")
+			usr, err := strconv.Atoi(m.Text[:index])
+			if err != nil {
+				b.Send(&tb.User{ID: dono}, "erro ao pegar o id do usuario")
+				return
+			}
+			b.Send(&tb.User{ID: usr}, m.Text[index+1:])
+		} else {
+			b.Send(&tb.User{ID: dono}, "["+strconv.Itoa(m.Sender.ID)+"] (- "+m.Sender.Username+" - "+m.Sender.FirstName+" "+m.Sender.LastName+") "+m.Text)
+		}
+	})
+
+	b.Start()
+}
